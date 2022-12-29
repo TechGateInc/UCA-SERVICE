90c58194857996ed740703ced61565f4fbb46e3e
Alabi <alabiemmanuel177@gmail.com>
2022-12-29 20:13:23 +0100

    Updated routes on the MainRoute Middleware

--- routes/main.js
+++ routes/main.js
@@ -1,16 +1,26 @@
 const helpers = require("../helpers/auth.guard");
-const AdminRouter = require("./admin"); 
+const AdminRouter = require("./admin");
 const LecturerRouter = require("./lecturer");
 const StudentRouter = require("./student");
-const LoginRouter = require("./auth")
-const LocationRouter = require("./location")
+const LoginRouter = require("./auth");
+const LocationRouter = require("./location");
+const AttendanceRouter = require("./attendance");
+const CourseRouter = require("./course");
+const DepartmentRouter = require("./department");
+const TimetableRouter = require("./timetable");
+const VenueRouter = require("./venue");
 
 const mainRoute = require("express").Router();
 
-mainRoute.use('/', LoginRouter);
-mainRoute.use('/student', StudentRouter);
-mainRoute.use('/admin', AdminRouter);
-mainRoute.use('/lecturer', LecturerRouter);
-mainRoute.use('/location', LocationRouter);
+mainRoute.use("/", LoginRouter);
+mainRoute.use("/student", StudentRouter);
+mainRoute.use("/admin", AdminRouter);
+mainRoute.use("/lecturer", LecturerRouter);
+mainRoute.use("/attendance", AttendanceRouter);
+mainRoute.use("/location", LocationRouter);
+mainRoute.use("/course", CourseRouter);
+mainRoute.use("/department", DepartmentRouter);
+mainRoute.use("/timetable", TimetableRouter);
+mainRoute.use("/venue", VenueRouter);
 
 module.exports = mainRoute;
